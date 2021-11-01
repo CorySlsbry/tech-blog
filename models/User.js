7f74e42e9a33416fd3dc41a74c950a48cfadaa36
CorySlsbry <87287656+CorySlsbry@users.noreply.github.com>
2021-10-31 20:52:40 -0500

    api routes created

--- models/User.js
+++ models/User.js
@@ -31,9 +31,7 @@ User.init(
     }
   },
   {
-    hooks: {
-      // set up beforeCreate lifecycle hook functionality
-      async beforeCreate(newUserData) {
+    hooks: {      async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
@@ -42,15 +40,11 @@ User.init(
         return updatedUserData;
       }
     },  
-    //pass in the imported sequelize connection
     sequelize,
-    // don't automatically create timestamp fields
     timestamps: false,
-    // don't plurallize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing
     underscored: true,
-    // make it so our model name stays in lowercase in the database
+    
     modelName: 'user'
   }
 );
